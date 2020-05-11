046ed305abfa096265c77d842d8a667e02f17c79
jasminluka <jasminluka007@gmail.com>
2020-05-11 14:02:30 +0200

    Created Home content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import HomeBanner from '../components/homePage/HomeBanner';
+import HomeContent from '../components/homePage/HomeContent';
 import { useGlobalStateContext, useGlobalDispatchContext } from '../context/globalContext';
 
 const IndexPage = props => {
@@ -16,6 +17,7 @@ const IndexPage = props => {
   return (
     <Layout>
       <HomeBanner onCursor={onCursor} />
+      <HomeContent />
     </Layout>
   );
 }
--- src/styles/homeStyles.js
+++ src/styles/homeStyles.js
@@ -39,3 +39,16 @@ export const Headline = styled(motion.span)`
   font-weight: 900;
   line-height: .76;
 `;
+
+// Content Section
+export const HomeContentSection = styled(motion.div)`
+  margin-bottom: 200px;
+`;
+
+export const Content = styled.h2`
+  width: 53%;
+  font-size: 2.3rem;
+  font-weight: 400;
+  margin-left: 124px;
+  color: ${props => props.theme.text};
+`;
